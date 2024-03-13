e725cd77b41cb4ccfec2f1ade9950e47cd0c073d
iPadla <petrozavr@ya.ru>
2024-03-13 13:31:12 +0300

    fix

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b, 2) - 4 * a * c;
   
-  if (d < 0) {
-  arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
   arr = [-b / (2 * a)];
   } else if (d > 0) {
   arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
@@ -15,14 +13,8 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   percent = percent / 100
-  console.log(percent)
-  let P, S, monthlyPayment, result;
-  P = percent / 12
-  console.log(P)
-  S = amount - contribution
-  console.log(S)
-  monthlyPayment = S * (P + (P / ((Math.pow((1 + P), countMonths)) - 1)))
-  console.log(monthlyPayment)
-  result = +(monthlyPayment * countMonths).toFixed(2)
-  return result;
+  let percentPerMonth = percent / 12,
+  totalCreditAmount = amount - contribution,
+  monthlyPayment = totalCreditAmount * (percentPerMonth + (percentPerMonth / ((Math.pow((1 + percentPerMonth), countMonths)) - 1)));
+  return +(monthlyPayment * countMonths).toFixed(2);
 }
