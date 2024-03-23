118148d90279f276ba98504aeb88c54032c8f214
iPadla <petrozavr@ya.ru>
2024-03-23 21:29:38 +0300

    tired

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,20 +1,22 @@
-//функция получает числа
 function getArrayParams(...arr) {
   return { min: Math.min(...arr), max: Math.max(...arr), avg: +(arr.reduce((a, b) => (a + b), 0) / arr.length).toFixed(2) };
 }
 
 function summElementsWorker(...arr) {
   if(!arr.length) return 0
+
   return +arr.reduce((a, b) => (a + b), 0).toFixed(2);
 }
 
 function differenceMaxMinWorker(...arr) {
   if(!arr.length) return 0
+
   return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
   if(!arr.length) return 0
+
   let sumEvenElement = 0, sumOddElement = 0;
   for(i = 0; i < arr.length; i++) {
     arr[i] % 2 === 0 ? (sumEvenElement += arr[i]) : (sumOddElement += arr[i])
@@ -24,6 +26,7 @@ return sumEvenElement - sumOddElement;
 
 function averageEvenElementsWorker(...arr) {
   if(!arr.length) return 0
+
   let accum = 0, counter = 0;
   for(i = 0; i < arr.length; i++) {
     if(arr[i] % 2 === 0) accum += arr[i], counter++
