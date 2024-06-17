2c16cc84e07d744f4a57616336df0505be92e38e
iPadla <petrozavr@ya.ru>
2024-06-17 23:19:16 +0300

    0.1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,25 @@
 function Student(name, gender, age) {
-  
+  (this.name = name), (this.gender = age), (this.age = age), (marks = []);
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.hasOwnProperty("marks")) {
+    this[marks] = [...this[marks], marks];
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (!this.hasOwnProperty("marks") || !this.marks?.length) {
+    return (
+      this.marks.reduce((prev, mark) => prev + mark, 0) / this.marks.length
+    );
+  }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  (this.excluded = reason), delete this.subject, delete this.marks;
+};
