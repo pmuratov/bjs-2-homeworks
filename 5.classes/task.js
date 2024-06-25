a6295bd88b4d5ce17a676973e5baf22b54864d53
iPadla <petrozavr@ya.ru>
2024-06-25 23:37:28 +0300

    arrgh

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -26,34 +26,6 @@ class PrintEditionItem {
   }
 }
 
-class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
-    this.state = 100;
-    this.type = null;
-  }
-
-  fix() {
-    this.state *= 1.5;
-  }
-
-  set state(digit) {
-    if (digit <= 0) {
-      this._state = 0;
-    } else if (digit >= 100) {
-      this._state = 100;
-    } else {
-      this._state = digit;
-    }
-  }
-
-  get state() {
-    return this._state;
-  }
-}
-
 class Magazine extends PrintEditionItem {
   constructor(name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
