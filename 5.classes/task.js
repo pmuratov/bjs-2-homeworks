ed2122d3fa16edbb05849af9bb3d29c9f2d236fe
iPadla <petrozavr@ya.ru>
2024-06-25 23:36:04 +0300

    arrgh

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -12,7 +12,7 @@ class PrintEditionItem {
   }
 
   set state(digit) {
-    if (digit < 0) {
+    if (digit <= 0) {
       this._state = 0;
     } else if (digit >= 100) {
       this._state = 100;
@@ -23,5 +23,72 @@ class PrintEditionItem {
 
   get state() {
     return this._state;
+  }
+}
+
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set state(digit) {
+    if (digit <= 0) {
+      this._state = 0;
+    } else if (digit >= 100) {
+      this._state = 100;
+    } else {
+      this._state = digit;
+    }
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "novelBook";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "fantasticBook";
+  }
+}
+
+class Detective extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "detective";
   }
 }
