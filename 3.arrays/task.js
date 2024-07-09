177acaee5c9631a8888b36e6bdbc84e8e9c47c1b
iPadla <petrozavr@ya.ru>
2024-07-09 20:09:07 +0300

    .

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every(function (element, index) {
+    return element === arr2[index] && arr1.length === arr2.length;
+  });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let peopleFiltered = users.filter((person) => person.gender === gender);
+
+  if (peopleFiltered.length) {
+    return (
+      peopleFiltered.reduce((acc, person) => acc + person.age, 0) /
+      peopleFiltered.length
+    );
+  } else {
+    return 0;
+  }
 }
