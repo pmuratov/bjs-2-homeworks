840e5da23aa87072aac8d1797114cb3c915253dc
iPadla <petrozavr@ya.ru>
2024-07-10 21:53:29 +0300

    1st task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(parseValue) {
+  let result = Number.parseFloat(parseValue);
+  if (isNaN(result)) {
+    throw new Error("Невалидное значение");
+  } else {
+    return result;
+  }
+}
+
+function validateCount(parseValue) {
+  try {
+    return parseCount(parseValue);
+  } catch (error) {
+    return error;
+  }
+}
